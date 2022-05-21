3d2a484414e2932034a0c68a01ca6e7ae19866e2
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2022-05-21 15:07:10 -0300

    Adding Spring App class and docker files

---
+++ Facu/Cuarto/Ing Sistemas/JibberJabber/src/test/java/com/ingsis/JibberJabber/JibberJabberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.JibberJabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
